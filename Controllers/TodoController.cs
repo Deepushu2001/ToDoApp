c03b4e0a69a2026b278cf233879c5bd146326150
Deepushu2001 <97843912+Deepushu2001@users.noreply.github.com>
2024-02-13 12:16:42 +0530

    Controoler

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo_App.Data;
+using ToDo_App.Models;
+
+namespace ToDo_App.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoRepository _repository;
+
+        public TodoController(TodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var todos = _repository.GetAllTodos();
+            return View(todos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.AddTodoItem(todoItem);
+                return RedirectToAction("Index");
+            }
+            return View(todoItem);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var todoItem = _repository.GetTodoById(id);
+            return View(todoItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.UpdateTodoItem(todoItem);
+                return RedirectToAction("Index");
+            }
+            return View(todoItem);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var todoItem = _repository.GetTodoById(id);
+            return View(todoItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _repository.DeleteTodoItem(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
